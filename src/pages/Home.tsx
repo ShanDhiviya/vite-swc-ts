b6bf2f72cd51aa0c701872c5a693c894a412ba28
ShanDhiviya <132169598+ShanDhiviya@users.noreply.github.com>
2025-04-19 01:55:25 +0530

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,9 @@
 const Home = () => {
   return (
     <div className="text-center p-4">
+      <h1>
+        Release Branch
+      </h1>
       <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
       <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.</p>
     </div>
